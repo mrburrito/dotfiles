61ef5209ad1f6819f81ee099152e119a292b8280
Gordon Shankman <gordon.shankman@bti360.com>
2016-10-26 10:26:47 -0400

    Added .gitignore. Updated python script, adding mkvenv3 function to support python3 virtual environments. Updated venv aliases so they recreate python3 environments properly.

--- profile/profile.d/python.sh
+++ profile/profile.d/python.sh
@@ -1,43 +1,63 @@
 VIRTUALENV_HOME=~/.pyenv
 
 function venv_alias {
-    venv=$1
-    name=`basename $venv`
-    alias v_$name="source $venv/bin/activate"
-    alias v_${name}_refresh="deactivate 2>&1 >/dev/null; rmvenv $name && mkvenv $name && source $venv/bin/activate"
-    alias v_${name}_lib="ls $venv/lib/python*/site-packages"
+    mkvenv_func=$1
+    venv=$2
+    name=`basename ${venv}`
+    alias v_${name}="source ${venv}/bin/activate"
+    alias v_${name}_refresh="deactivate 2>&1 >/dev/null; rmvenv ${name} && ${mkvenv_func} ${name} && source ${venv}/bin/activate"
+    alias v_${name}_lib="ls ${venv}/lib/python*/site-packages"
 }
 
-for venv in $VIRTUALENV_HOME/*; do venv_alias $venv; done
-alias lv='ls $VIRTUALENV_HOME'
+for venv in ${VIRTUALENV_HOME}/*; do
+    if [[ -f ${venv}/.mkvenv ]]; then
+        mkvenv_func=$(cat ${venv}/.mkvenv)
+    else
+        mkvenv_func=mkvenv
+    fi
+    venv_alias ${mkvenv_func} ${venv};
+done
+alias lv='ls ${VIRTUALENV_HOME}'
 
-function mkvenv {
+function _mkvenv {
+    mkvenv_func=$1
+    shift
 	name=$1
-	if [ -d $VIRTUALENV_HOME/$name ]; then
-		echo "virtualenv $name already exists"
+	venv_dir=${VIRTUALENV_HOME}/${name}
+    shift
+	if [ -d ${venv_dir} ]; then
+		echo "virtualenv ${name} already exists"
 		return 1
 	fi
-	virtualenv $VIRTUALENV_HOME/$name
-	venv_alias $VIRTUALENV_HOME/$name
-        source $VIRTUALENV_HOME/$name/bin/activate
-        pip install -U pybuilder
+	virtualenv ${venv_dir} $*
+	echo "${mkvenv_func}" > ${venv_dir}/.mkvenv
+	venv_alias ${mkvenv_func} ${venv_dir}
+    source ${venv_dir}/bin/activate
+    pip install -U pybuilder
+}
+
+function mkvenv {
+    _mkvenv mkvenv $1
+}
+
+function mkvenv3 {
+    _mkvenv mkvenv3 $1 -p python3
 }
 
 function rmvenv {
 	name=$1
-	if [ -d $VIRTUALENV_HOME/$name ]; then
-		echo "Removing virtualenv $name"
-		rm -rf $VIRTUALENV_HOME/$name
-		unalias v_$name
+	if [ -d ${VIRTUALENV_HOME}/${name} ]; then
+		echo "Removing virtualenv ${name}"
+		rm -rf ${VIRTUALENV_HOME}/${name}
+		unalias v_${name}
 		unalias v_${name}_refresh
 		unalias v_${name}_lib
 	else
-		echo "virturalenv $name does not exist"
+		echo "virturalenv ${name} does not exist"
 	fi
 }
 
 PIPSI_HOME=~/.pipsi-local
 PIPSI_BIN=~/bin
 export PIPSI_HOME PIPSI_BIN
-alias pipsi='pipsi --home=$PIPSI_HOME --bin-dir=$PIPSI_BIN'
-
+alias pipsi='pipsi --home=${PIPSI_HOME} --bin-dir=${PIPSI_BIN}'
