f4fd98e13dd9f90c48282cadadcfe114c64e1811
Gordon Shankman <gshankman@gmail.com>
2018-05-01 17:11:41 -0400

    op-password now checks for login and initiates a login before executing if necessary

--- profile/profile.d/1password.sh
+++ profile/profile.d/1password.sh
@@ -1,1 +1,9 @@
-alias oplogin='op signin ${ONE_PASSWORD_DOMAIN}'
+alias op-login='eval $(op signin ${ONE_PASSWORD_DOMAIN})'
+
+op-password() {
+  if [ -z "$(eval "echo \${OP_SESSION_${ONE_PASSWORD_DOMAIN}}")" ]; then
+    op-login
+  fi
+  local item="$@"
+  op get item "${item}" | jq -r .details.password
+}
