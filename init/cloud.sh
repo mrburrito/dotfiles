9491908b7f2f10449b2d42b41de374a5482750fb
Gordon Shankman <gshankman@gmail.com>
2018-06-16 22:03:52 -0400

    Removing Google Drive installation

--- init/cloud.sh
+++ init/cloud.sh
@@ -7,8 +7,9 @@ echo "Installing Dropbox"
 brew cask install dropbox
 
 # Install Google Drive
-echo "Installing Google Drive"
-brew cask install google-drive
+#echo "Installing Google Drive"
+#brew cask install google-drive
 
-echo "Sign in to Dropbox and Google Drive"
+#echo "Sign in to Dropbox and Google Drive"
+echo "Sign in to Dropbox"
 read -p "Press [Return] when sync is complete..."
