d45ab9e9e4e2c549508e61d736a79119600b90de
Gordon Shankman <gshankman@gmail.com>
2019-01-12 11:24:37 -0500

    Removing MAVEN_OPTS configuration based on version since java6 is no longer installed.

--- profile/profile.d/java.sh
+++ profile/profile.d/java.sh
@@ -1,49 +1,21 @@
-function maven_opts {
-	J8_MAVEN_OPTS="-Xmx2g"
-	J6_MAVEN_OPTS="$J8_MAVEN_OPTS -XX:MaxPermSize=512m"
-	# Configure Maven options based on Java version; Java >= 1.8 does not support -XX:MaxPermGen
-	if type -p java 2>&1 > /dev/null; then
-		_java=java
-	elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]]; then
-		_java="$JAVA_HOME/bin/java"
-	else
-		echo "No Java Found"
-	fi
-	if [[ "$_java" ]]; then
-    JAVA_VERSION=$("$_java" -version 2>&1 | sed 's/java version "\(.*\)".*/\1/; 1q')
-    if echo ${JAVA_VERSION} | grep \\.; then
-      MAJOR=$(echo ${JAVA_VERSION} | cut -f 1 -d '.')
-      MINOR=$(echo ${JAVA_VERSION} | cut -f 2 -d '.')
-    else
-      MAJOR=${JAVA_VERSION}
-      MINOR=0
-    fi
-    if [ ${MAJOR} -gt 1 -o ${MINOR} -gt 7 ]; then
-      MAVEN_OPTS=${J8_MAVEN_OPTS}
-    else
-      MAVEN_OPTS=${J6_MAVEN_OPTS}
-    fi
-	fi
-	export MAVEN_OPTS
-}
+#!/usr/bin/env bash
 
 alias jv='java -version'
 function switch_java {
 	export JAVA_HOME=`/usr/libexec/java_home -v $1`
-	maven_opts
 	java -version
 }
 
 JAVA_VERSIONS=`/usr/libexec/java_home -V 2>&1 | grep -E "^ +[0-9]" | sed 's/^ *//' | cut -f 1,2 -d '.' | uniq`
 for version in ${JAVA_VERSIONS}; do
-        major=`echo ${version} | cut -f 1 -d '.' | cut -f 1 -d ','`
+  major=`echo ${version} | cut -f 1 -d '.' | cut -f 1 -d ','`
 	minor=`echo ${version} | cut -f 2 -d '.'`
-        if [ ${major} -gt 1 ]; then
+  if [[ ${major} -gt 1 ]]; then
  	  set_alias="alias j${major}='switch_java ${version}'"
-        else
+  else
 	  set_alias="alias j${minor}='switch_java ${version}'"
-        fi
+  fi
 	eval ${set_alias}
 done
 
-maven_opts
+export MAVEN_OPTS="-Xmx2g"
