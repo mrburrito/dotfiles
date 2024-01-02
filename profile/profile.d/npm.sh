008f31058318dd3eb95ba586da3ddbc3cfc7b0fd
Gordon Shankman <gordon.shankman@bti360.com>
2024-01-02 14:16:13 -0500

    Update bash completion

--- profile/profile.d/npm.sh
+++ profile/profile.d/npm.sh
@@ -1,2 +1,5 @@
 alias npe='PATH="$(npm bin):$PATH"'
 
+export NVM_DIR="$HOME/.nvm"
+[[ -s "/opt/homebrew/opt/nvm/nvm.sh" ]] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ]] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
